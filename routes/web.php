168dad920cd2475b91cfd810fcf9f7529ea61ca6
Madhab <madhabkumarjoy@gmail.com>
2024-06-22 16:54:22 +0600

    2nd commit

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,5 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/profile/{id}',[ProfileController::class,'index']);
