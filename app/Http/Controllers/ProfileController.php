ca1672c3f33de22d55002f63fa616369032b7ec1
Madhab <madhabkumarjoy@gmail.com>
2024-06-22 16:50:46 +0600

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id){
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data=[
+            'id'=>$id,
+            'name'=>$name,
+            'age'=>$age,
+        ];
+        
+        $cookiename = "access_token";
+        $value = "123-XYZ";
+        $minutes = "1";
+        $path = "/";
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        return response()->json($data,200)->cookie($cookiename,$value,$minutes,$path,$domain,$secure,$httpOnly);
+   
+    }
+}
